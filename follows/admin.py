ff8ee12c78b8d802481c213523e3a739c4d942be
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-05-09 16:49:36 +0300

    Created models and admin panel to users, follows, posts

--- follows/admin.py
+++ follows/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from follows.models import Follow
 
-# Register your models here.
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ("follower", "followed", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("follower__username", "followed__username")
